12c244988be79faa1782024056c4532d715c17a4
arterehov <arterehov@PC11069>
2022-10-31 18:01:10 +0300

    Add directory Filter

--- PrototypeConductor/Conductor/ViewModel/FilterPrototypeViewModel.cs
+++ PrototypeConductor/Conductor/ViewModel/FilterPrototypeViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿
+using PrototypeConductor.Model;
+using PrototypeConductor.Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PrototypeConductor.Conductor.ViewModel
+namespace PrototypeConductor.ViewModel
 {
-    internal class FilterPrototypeViewModel
+    public class FilterPrototypeViewModel : TreeViewItemViewModel
     {
+        public Database Database { get; set; }
+        public FilterPrototypeViewModel(PrototypeName prototypeName, FieldPrototypeViewModel parentField, Database database)
+           : base(parentField, false)
+        {
+            Database = database;
+        }
     }
 }
